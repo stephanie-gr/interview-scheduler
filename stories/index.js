d51db5d2fe8c5df61b1ce5627f8c2590d992deed
Stephanie Grant <stephanie@Stephanies-MacBook-Air.local>
2021-09-02 07:46:52 -0700

    created confirm, error, show, and status Appointment components

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,10 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,3 +142,7 @@ storiesOf("Button", module)
         .add("Appointment", () => <Appointment time="12pm" />)
         .add("Header", () => <Header time="12pm" />)
         .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+        .add("Show", () => <Show student="" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+        .add("Confirm", () => <Confirm message="" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
+        .add("Status", () => <Status message="Deleting"/>)
+        .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
