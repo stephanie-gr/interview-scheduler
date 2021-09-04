51b073c7b7277aed33d80b4790517fed0dad8b16
Stephanie Grant <stephanie@Stephanies-MacBook-Air.local>
2021-09-04 10:23:45 -0700

    created transition functions to track mode in appointment component

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition (mode, replace = false) {
+    if (!replace) {
+      setHistory(prev => {
+        return [...prev, mode]
+      })
+      setMode(mode);
+    } else {
+      setHistory(prev => {
+        return [...prev.slice(0, prev.length - 1), mode]
+      })
+      setMode(mode);
+    }
+  }     
+
+  function back () {
+    if (history.length > 1 ) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  }
+      
+
+  return { mode, transition, back };
+}
