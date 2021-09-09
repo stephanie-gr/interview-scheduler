94e1e414d5a201cddb3fa073b220f8878f84a3bb
Stephanie Grant <stephanie@Stephanies-MacBook-Air.local>
2021-09-08 17:49:21 -0700

    screenshots

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,10 +5,10 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-  
+    
     cy.contains("[data-testid=day]", "Tuesday")
-      .click()
-      .should("have.class", "day-list__item--selected")
+    .click()
+    .should("have.class", "day-list__item--selected")
   });
 
 });
